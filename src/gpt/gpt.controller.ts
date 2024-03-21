6bc80db91e665c2a5dc6ec87ba5f2ed59c41c769
Stefania Diana <steefaniadiana@gmail.com>
2024-03-21 13:35:41 +0100

    versiondiana

--- src/gpt/gpt.controller.ts
+++ src/gpt/gpt.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { Body, Controller, FileTypeValidator, Get, HttpStatus, MaxFileSizeValidator, Param, ParseFilePipe, Post, Res, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { GptService } from './gpt.service';
-import { OrthographyDto, ProsConsDiscusserDto, TranslateDto } from './dtos';
-import { Response } from 'express';
+import { AudioToTextDto, ImageGenerationDto, ImageVariationDto, OrthographyDto, ProsConsDiscusserDto, TextToAudioDto, TranslateDto } from './dtos';
+import type { Response } from 'express';
+import { diskStorage } from 'multer';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('gpt')
 export class GptController {
@@ -50,5 +52,89 @@ TranslateText (
   ) {
   return this.gptService.translateText(translateDto);
 }
+
+@Post('text-to-audio')
+async textToAudio ( 
+  @Body() textToAudioDto:TextToAudioDto,
+  @Res() res: Response
+  ) {
+
+const filePath = await this.gptService.textToAudio(textToAudioDto)
+
+res.setHeader('Content-Type', 'audio/mp3')
+res.status(HttpStatus.OK)
+res.sendFile(filePath);
+  }
+
+  @Get('text-to-audio/:field')
+  async textToAudioGetter ( 
+    @Res() res: Response,
+    @Param('field') field:string
+    ) {
+  
+    const filePath = await this.gptService.textToAudioGetter(field);
+    res.setHeader('Content-Type', 'audio/mp3');
+    res.status(HttpStatus.OK);
+    res.sendFile(filePath);
+    }
+
+    @Post('audio-to-text')
+    @UseInterceptors(
+      FileInterceptor ('file', {
+        storage: diskStorage({
+          destination: './generated/upload',
+          filename: (req, file, callback) => {
+            const fileExtension = file.originalname.split('.').pop();
+            const fileName = `${ new Date().getTime() }.${ fileExtension }`;
+            return callback( null, fileName )
+          }
+        })
+      })
+    )
+    async audioToText( 
+      @UploadedFile(
+        new ParseFilePipe ({
+          validators: [
+            new MaxFileSizeValidator({maxSize: 1000 * 1024 * 5, message: 'File is bigger than 5mb'}),
+            new FileTypeValidator({fileType: 'audio/*'})
+          ]
+        })
+      ) file: Express.Multer.File,
+
+      @Body() audioToTextDto: AudioToTextDto,
+      
+    ) {
+      return this.gptService.audioToText( file, audioToTextDto )
+      
+    }
+@Post('image-generation')
+async imageGeneration(
+  @Body() imageGenerationDto: ImageGenerationDto
+) {
+  return await this.gptService.imageGeneration( imageGenerationDto )
+}
+
+@Get('image-generation/:fileName')
+async getGeneratedImage(
+  @Res() res: Response,
+  @Param('fileName') fileName:string,
+) {
+  const filePath = await this.gptService.getGeneratedImage( fileName );
+
+  res.sendFile( filePath );
+}
+
+@Post('image-variation')
+async imageVariation( 
+   @Body() imageVariation: ImageVariationDto
+) {
+return await this.gptService.generateImageVariation( imageVariation )
+}
+
+
+
+
+
+
 
 }
--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,10 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { orthographyCheckUseCase } from './use-cases/orthography.use-cases';
-import { OrthographyDto, ProsConsDiscusserDto, TranslateDto } from './dtos';
+import { AudioToTextDto, ImageGenerationDto, ImageVariationDto, OrthographyDto, ProsConsDiscusserDto, TranslateDto } from './dtos';
 import OpenAI from 'openai';
 import { prosConsDiscusserUseCase } from './use-cases/pros-cons-discusser.use-case';
 import { prosConsDiscusserStreamUseCase } from './use-cases/pros-cons-stream.use-case';
-import { translateUseCase } from './use-cases';
+import { audioToTextUseCase, imageGenerationUseCase, translateUseCase } from './use-cases';
+import { TextToAudioDto } from './dtos/text-to-audio.dto';
+import { textToAudioUseCase } from './use-cases/text-to-audio.use-case';
+import * as path from 'path'
+import * as fs from 'fs'
+import { imageVariationUseCase } from './use-cases/image-variation.use-case';
 
 @Injectable()
 export class GptService {
@@ -26,7 +31,50 @@ export class GptService {
     async prosConsDiscusserStream ( { prompt }: ProsConsDiscusserDto) {
         return await prosConsDiscusserStreamUseCase( this.openai, {prompt} );
     }
+
     async translateText ( { prompt, lang }: TranslateDto) {
         return await translateUseCase ( this.openai, {prompt, lang} );
     }
+
+    async textToAudio ( { prompt, voice }: TextToAudioDto) {
+        return await textToAudioUseCase ( this.openai, {prompt, voice} );
+    }
+
+    async textToAudioGetter( field: string ) {
+        const filePath = path.resolve( __dirname, '../../generated/audios/', `${ field }.mp3`); 
+        const wasFound = fs.existsSync( filePath );
+
+         if ( !wasFound ) throw new NotFoundException(`El archivo no está, churri :(`)
+         return filePath;
+    }
+
+    async audioToText(audioFile: Express.Multer.File, audioToTextDto: AudioToTextDto ) {
+
+        const { prompt } = audioToTextDto;
+
+        return await audioToTextUseCase( this.openai, { audioFile, prompt } );
+    }
+
+    async imageGeneration(imageGenerationDto: ImageGenerationDto) {
+
+        return imageGenerationUseCase( this.openai, { ...imageGenerationDto } )
+    }
+
+    async getGeneratedImage(fileName: string) {
+
+        const filePath = path.resolve(  './', './generated/images/', fileName ); 
+        const exists = fs.existsSync( filePath );
+
+        if ( !exists ) throw new NotFoundException(`File ${ fileName } not found`)
+
+        return filePath;
+
+    }
+
+    async generateImageVariation({ baseImage }: ImageVariationDto) {
+        return imageVariationUseCase( this.openai, { baseImage });
+    }
+
+
+
 }
